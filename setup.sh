2176c47c2f81035ca5c338f2de761fabb94c7ce5
Mido <ahmed.mohamed-ali@mobile-devices.fr>
2016-09-29 21:21:10 +0200

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cd ruby_model_generator && bundle install --path .gems
-cd ruby_notifier_server && bundle install --path .gems
-cd simpleGRPCWatcherClient && ./generate_jar.sh
+cd ruby_model_generator && bundle install --path .gems; cd ../
+cd ruby_notifier_server && bundle install --path .gems; cd ../
+cd simpleGRPCWatcherClient && ./generate_jar.sh; cd ../
